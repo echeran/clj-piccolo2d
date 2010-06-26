41ca21abd985e58ae14ceb5246be60287187fd61
Fabian Aussems <fabianaussems@gmail.com>
2010-06-26 15:42:04 +0200

    added piccolo2d-extras as dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject overtone/clj-piccolo2d "0.0.1-SNAPSHOT"
+(defproject overtone/clj-piccolo2d "0.0.2-SNAPSHOT"
   :description "Clojure bindings for piccolo2d"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [org.piccolo2d/piccolo2d-core "1.3"]
+                 [org.piccolo2d/piccolo2d-extras "1.3"]
                  ]
   :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
