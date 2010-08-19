9544092fa402184d94eda66e5fcf370cf300ca71
Fabian Aussems <fabianaussems@gmail.com>
2010-08-19 22:20:58 +0200

    upped version number and switched to clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clj-piccolo2d "0.0.3-SNAPSHOT"
+(defproject clj-piccolo2d "0.0.4-SNAPSHOT"
   :description "Clojure bindings for piccolo2d"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.piccolo2d/piccolo2d-core "1.3"]
                  [org.piccolo2d/piccolo2d-extras "1.3"]
                  ]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+                     [swank-clojure "1.2.1"]
+                     ])
