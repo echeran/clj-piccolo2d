fbf983a507ec604f05c3305a963aff38d5053357
Fabian Aussems <fabianaussems@gmail.com>
2010-09-22 14:00:17 +0200

    removed dev deps

--- project.clj
+++ project.clj
@@ -2,9 +2,7 @@
   :description "Clojure bindings for piccolo2d"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 
                  [org.piccolo2d/piccolo2d-core "1.3"]
                  [org.piccolo2d/piccolo2d-extras "1.3"]
-                 ]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [swank-clojure "1.2.1"]
-                     ])
+                 ])
