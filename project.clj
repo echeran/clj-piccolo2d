2ca8b0e26020ca01987f29302180db25661b4f1f
Fabian Aussems <fabianaussems@gmail.com>
2011-03-13 23:36:10 +0100

    Upped versio number and added arotate! ascale! and use reify instead of proxy

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
-(defproject clj-piccolo2d "0.0.4-SNAPSHOT"
+(defproject clj-piccolo2d "0.0.5-SNAPSHOT"
   :description "Clojure bindings for piccolo2d"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 
                  [org.piccolo2d/piccolo2d-core "1.3"]
-                 [org.piccolo2d/piccolo2d-extras "1.3"]
-                 ])
+                 [org.piccolo2d/piccolo2d-extras "1.3"]])
--- src/clj_piccolo2d/core.clj
+++ src/clj_piccolo2d/core.clj
@@ -1,52 +1,19 @@
-(ns clj-piccolo2d.core
+(ns
+    ^{:author "Fabian Aussems"}
+  clj-piccolo2d.core
   (:import
-   (java.awt BasicStroke
-             Stroke
-             BorderLayout
-             Color
-             Point
-             Dimension
-             Font
-             Insets
-             RenderingHints
-             Shape)
-   (edu.umd.cs.piccolo PCamera
-                       PCanvas
-                       PInputManager
-                       PLayer
-                       PNode
-                       PRoot)
-   (edu.umd.cs.piccolo.activities PActivity
-                             PActivityScheduler
-                             PColorActivity
-                             PInterpolatingActivity
-                             PTransformActivity)
-   (edu.umd.cs.piccolo.event PInputEventListener
-                             PBasicInputEventHandler
-                             PDragEventHandler
-                             PDragSequenceEventHandler
-                             PInputEvent
-                             PInputEventFilter
-                             PPanEventHandler
-                             PZoomEventHandler)
-   (edu.umd.cs.piccolo.nodes PHtmlView
-                             PImage
-                             PPath
-                             PText)
-   (edu.umd.cs.piccolo.util PNodeFilter
-                            PAffineTransform
-                            PBounds
-                            PDebug
-                            PDimension
-                            PObjectOutputStream
-                            PPaintContext
-                            PPickPath
-                            PStack
-                            PUtil)
-   (edu.umd.cs.piccolox.pswing PSwingCanvas
-                               PSwing)))
+   (java.awt BasicStroke Stroke BorderLayout Color Point Dimension Font Insets RenderingHints Shape)
+   (edu.umd.cs.piccolo PCamera PCanvas PInputManager PLayer PNode PRoot)
+   (edu.umd.cs.piccolo.activities PActivity PActivityScheduler PColorActivity PInterpolatingActivity PTransformActivity)
+   (edu.umd.cs.piccolo.event PInputEventListener PBasicInputEventHandler PDragEventHandler PDragSequenceEventHandler PInputEvent PInputEventFilter PPanEventHandler PZoomEventHandler)
+   (edu.umd.cs.piccolo.nodes PHtmlView PImage PPath PText)
+   (edu.umd.cs.piccolo.util PNodeFilter PAffineTransform PBounds PDebug PDimension PObjectOutputStream PPaintContext PPickPath PStack PUtil)
+   (edu.umd.cs.piccolox.pswing PSwingCanvas PSwing)))
 
-;; general
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (derive java.lang.Float ::number)
 (derive java.lang.Double ::number)
 (derive java.lang.Integer ::number)
@@ -56,13 +23,17 @@
 (defn layer [] (PLayer.))
 (defn canvas [] (PCanvas.))
 
-;; picking
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Picking
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn picked-node [event] (.getPickedNode event))
 (defn handled! [event] (.setHandled event true))
 (defn delta-relative-to [event node] (.getDeltaRelativeTo event node))
 
-;; swing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Swing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn repaint! [frame] (.repaint frame))
 
@@ -79,7 +50,10 @@
 
 (defn swingcanvas [] (PSwingCanvas.))
 
-;; bounds
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Bounds
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defmulti set-bounds! (fn [node x y w h] (type node)))
 (defmethod set-bounds! PNode [node x y w h] (.setBounds node x y w h ))
 
@@ -97,37 +71,44 @@
 
 (defmulti set-size! (fn [node w h] (type node)))
 (defmethod set-size! PNode [node w h]
-           (doto node
-             (.setWidth w)
-             (.setHeight h)))
-(defmethod set-size! PSwing [node w h]
-           (doto (.getComponent node)
-             (.setPreferredSize (Dimension. w h))
-             (.setVisible false)            ;; Hack to always resize component
-             (.setVisible true)))
+  (doto node
+    (.setWidth w)
+    (.setHeight h)))
 
+(defmethod set-size! PSwing [node w h]
+  (doto (.getComponent node)
+    (.setPreferredSize (Dimension. w h))
+    (.setVisible false)            ;; Hack to always resize component
+    (.setVisible true)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; add / remove /clear
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (derive PNode ::addable)
 (derive PPath ::addable)
 
-(defmulti add! (fn [parent child] [(type parent) (type child)]))
+(defmulti  add! (fn [parent child] [(type parent) (type child)]))
 (defmethod add! [PLayer ::addable] [layer node] (.addChild layer node))
 (defmethod add! [PNode ::addable] [parent child] (.addChild parent child))
 (defmethod add! [PCanvas ::addable] [parent child] (.addChild (.getLayer parent) child ))
 
-(defmulti remove! (fn [parent child] [(type parent) (type child)]))
+(defmulti  remove! (fn [parent child] [(type parent) (type child)]))
 (defmethod remove! [PLayer ::addable] [layer node] (.removeChild layer node))
 (defmethod remove! [PNode ::addable] [parent child] (.removeChild parent child))
 (defmethod remove! [PCanvas ::addable] [parent child] (.removeChild (.getLayer parent) child ))
 
 ;;(defmethod remove! [PNode nil] [node] (.removeChild (.getParent node) node))
 
-(defmulti clear! (fn [node] (type node)))
+(defmulti  clear! (fn [node] (type node)))
 (defmethod clear! PNode [node] (.removeAllChildren node))
 (defmethod clear! PLayer [layer] (.removeAllChildren layer))
 (defmethod clear! PCanvas [canvas] (.removeAllChildren (.getLayer canvas)))
 
-;; transformation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Transformation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defmulti translate! (fn [node x y] (type node)))
 (defmethod translate! PNode [node x y] (.translate node x y))
 
@@ -135,40 +116,48 @@
 (defmethod translate PNode [x y node] (doto (PNode.) (translate! x y) (add! node)))
 
 (defmulti atranslate! (fn [node x y] (type node)))
-(defmethod atranslate! PNode [node x y]
-  (.setTransform node nil)
-  (.translate node x y))
+(defmethod atranslate! PNode [node x y] (.setTransform node nil) (.translate node x y))
 
 (defmulti scale! (fn [node value] (type node)))
 (defmethod scale! PNode [node value] (.scale node value))
 
-(defmulti rotate! (fn [node rotation] (type node)))
-(defmethod rotate! PNode [node rotation] (.rotate node rotation))
-
-
 (defmulti scale (fn [value node] (type node)))
 (defmethod scale PNode [value node] (doto (PNode.) (scale! value) (add! node)))
+
+(defmulti ascale! (fn [value node] (type node)))
+(defmethod ascale! PNode [value node] (.setTransform node nil) (.scale node value))
+
+(defmulti rotate! (fn [node rotation] (type node)))
+(defmethod rotate! PNode [node rotation] (.rotate node rotation))
 
 (defmulti rotate (fn [theta node] (type node)))
 (defmethod rotate PNode [theta node] (doto (PNode.) (rotate! theta) (add! node)))
 
-;; information
+(defmulti arotate! (fn [node rotation] (type node)))
+(defmethod arotate! PNode [node rotation] (.setTransform node nil) (.rotate node rotation))
 
-(defmulti width type)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Information
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmulti  width type)
 (defmethod width PDimension [dimension] (.getWidth dimension))
 (defmethod width PNode [node] (.getWidth node))
 
-(defmulti height type)
+(defmulti  height type)
 (defmethod height PDimension [dimension] (.getHeight dimension))
 (defmethod height PNode [node] (.getHeight node))
 
-(defmulti x-offset type)
+(defmulti  x-offset type)
 (defmethod x-offset PNode [node] (.getXOffset node))
 
-(defmulti y-offset type)
+(defmulti  y-offset type)
 (defmethod y-offset PNode [node] (.getYOffset node))
 
-;; text
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Text
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn text
   ([] (PText.))
   ([txt] (PText. txt)))
@@ -181,12 +170,18 @@
                  (= style :italic) (Font/ITALIC))]
     (.setFont node (Font. name st (Integer. size)))))
 
-;; shapes
-(defn ellipse [x y w h] (PPath/createEllipse x y w h))
-(defn rectangle [x y w h] (PPath/createRectangle x y w h))
-(defn line [x1 y1 x2 y2] (PPath/createLine x1 y1 x2 y2))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Shapes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; strokes
+(defn ellipse [x y w h]   (PPath/createEllipse x y w h))
+(defn rectangle [x y w h] (PPath/createRectangle x y w h))
+(defn line [x1 y1 x2 y2]  (PPath/createLine x1 y1 x2 y2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Stroke
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def stroke-cap-map  {:butt   BasicStroke/CAP_BUTT
                       :round  BasicStroke/CAP_ROUND
                       :square BasicStroke/CAP_SQUARE})
@@ -202,15 +197,18 @@
 (derive Stroke ::stroke)
 
 (defmulti set-stroke! (fn [node & args] [(type node)
-                                         (type (nth args 0 nil))
-                                         (type (nth args 1 nil))
-                                         (type (nth args 2 nil))]))
+                                        (type (nth args 0 nil))
+                                        (type (nth args 1 nil))
+                                        (type (nth args 2 nil))]))
 
 (defmethod set-stroke! [PPath ::stroke nil nil] [node stroke] (.setStroke node stroke))
 (defmethod set-stroke! [PPath ::number nil nil] [node width] (.setStroke node (stroke width)))
 (defmethod set-stroke! [PPath ::number ::keyword ::keyword] [node width cap join] (.setStroke node (stroke width cap join)))
 
-;; paint
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Paint
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def color-map {:black      Color/BLACK
                 :blue       Color/BLUE
                 :cyan       Color/CYAN
@@ -232,10 +230,10 @@
 
 (derive Color ::color)
 (defmulti set-paint! (fn [node & args] [(type node)
-                                        (type (nth args 0 nil))
-                                        (type (nth args 1 nil))
-                                        (type (nth args 2 nil))
-                                        (type (nth args 3 nil))]))
+                                       (type (nth args 0 nil))
+                                       (type (nth args 1 nil))
+                                       (type (nth args 2 nil))
+                                       (type (nth args 3 nil))]))
 
 (defmethod set-paint! [PCanvas ::color nil nil nil] [node color] (.setBackground node color))
 (defmethod set-paint! [PCanvas ::keyword nil nil nil] [node key] (.setBackground node (color-map key)))
@@ -249,10 +247,10 @@
 
 
 (defmulti set-stroke-paint! (fn [node & args] [(type node)
-                                        (type (nth args 0 nil))
-                                        (type (nth args 1 nil))
-                                        (type (nth args 2 nil))
-                                        (type (nth args 3 nil))]))
+                                              (type (nth args 0 nil))
+                                              (type (nth args 1 nil))
+                                              (type (nth args 2 nil))
+                                              (type (nth args 3 nil))]))
 
 (defmethod set-stroke-paint! [PPath ::color nil nil nil] [node color] (.setStrokePaint node color))
 (defmethod set-stroke-paint! [PPath ::keyword nil nil nil] [node key] (.setStrokePaint node (color-map key)))
@@ -261,80 +259,81 @@
 
 
 (defmulti set-text-paint! (fn [node & args] [(type node)
-                                        (type (nth args 0 nil))
-                                        (type (nth args 1 nil))
-                                        (type (nth args 2 nil))
-                                        (type (nth args 3 nil))]))
+                                            (type (nth args 0 nil))
+                                            (type (nth args 1 nil))
+                                            (type (nth args 2 nil))
+                                            (type (nth args 3 nil))]))
 
 (defmethod set-text-paint! [PText ::color nil nil nil] [node color] (.setTextPaint node color))
 (defmethod set-text-paint! [PText ::keyword nil nil nil] [node key] (.setTextPaint node (color-map key)))
 (defmethod set-text-paint! [PText ::number ::number ::number nil] [node r g b] (.setTextPaint node (color r g b)))
 (defmethod set-text-paint! [PText ::number ::number ::number ::number] [node r g b a] (.setTextPaint node (color r g b a)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Events
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; events
+(defn on-mouse-clicked [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mouseClicked [event] (cb event)))))
 
-(defmacro on-mouse-clicked [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mouseClicked [event#] (~cb event#)))))
+(defn on-mouse-entered [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mouseEntered [event] (cb event)))))
 
-(defmacro on-mouse-entered [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mouseEntered [event#] (~cb event#)))))
+(defn on-mouse-exited [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mouseExited [event] (cb event)))))
 
-(defmacro on-mouse-exited [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mouseExited [event#] (~cb event#)))))
+(defn on-mouse-moved [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mouseMoved [event] (cb event)))))
 
-(defmacro on-mouse-moved [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mouseMoved [event#] (~cb event#)))))
+(defn on-mouse-wheel-rotated [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mouseWheelRotated [event] (cb event)))))
 
-(defmacro on-mouse-wheel-rotated [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mouseWheelRotated [event#] (~cb event#)))))
+(defn on-mouse-wheel-rotated-by-block [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mouseWheelRotatedByBlock [event] (cb event)))))
 
-(defmacro on-mouse-wheel-rotated-by-block [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mouseWheelRotatedByBlock [event#] (~cb event#)))))
+(defn on-mouse-pressed [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mousePressed [event] (cb event)))))
 
-(defmacro on-mouse-pressed [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mousePressed [event#] (~cb event#)))))
+(defn on-mouse-dragged [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mouseDragged [event] (cb event)))))
 
-(defmacro on-mouse-dragged [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mouseDragged [event#] (~cb event#)))))
+(defn on-mouse-released [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (mouseReleased [event] (cb event)))))
 
-(defmacro on-mouse-released [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (mouseReleased [event#] (~cb event#)))))
+(defn on-keyboard-focus-gained [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (keyboardFocusGained [event] (cb event)))))
 
-(defmacro on-keyboard-focus-gained [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (keyboardFocusGained [event#] (~cb event#)))))
+(defn on-keyboard-focus-lost [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (keyboardFocusLost [event] (cb event)))))
 
-(defmacro on-keyboard-focus-lost [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (keyboardFocusLost [event#] (~cb event#)))))
+(defn on-key-typed [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (keyTyped [event] (cb event)))))
 
-(defmacro on-key-typed [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (keyTyped [event#] (~cb event#)))))
-
-(defmacro on-key-pressed [component cb]
-  `(. ~component addInputEventListener
-      (proxy [PBasicInputEventHandler] []
-        (keyPressed [event#] (~cb event#)))))
+(defn on-key-pressed [component cb]
+  (.addInputEventListener component
+                          (reify PBasicInputEventHandler
+                            (keyPressed [event] (cb event)))))
